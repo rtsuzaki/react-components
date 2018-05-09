6b2a4048dc6fd07c691e01355e83f103b162c917
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-05-09 12:06:01 -0700

    Created grocery list that crosses off clicked items with React

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // TODO
+var GroceryList = (props) => (
+	  <ul>
+	    {props.items.map(item =>
+	    	<GroceryListItem item = {item} />
+	    )}
+	  </ul>
+);
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style = {style} onClick={this.onListItemClick.bind(this)}>{this.props.item}</li>
+    );
+  }
+}
+
+var App = () => (
+  <div>
+    <h2>My GroceryList</h2>
+    <GroceryList items ={['Cucumbers','Kale']}/>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("app"));
